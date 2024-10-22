cc00c19c029e754367dcdfed85a7e60d09d5cbac
BT_1N3_33 <idk@>
2024-10-22 09:32:59 -0400

    Set up Selector and the buttons, need to finish implementing the stopwatch functionaility itself

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.stopwatch
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var timeDisplayer: TextView
+    private lateinit var playbutton: ImageButton
+    private lateinit var pausebutton: ImageButton
+    private lateinit var resetbutton: ImageButton
+    var time = 0;
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+
+        val handler: Handler = Handler(Looper.getMainLooper())
+        var runnable: Runnable = Runnable { }
+        runnable = Runnable {
+            time++;
+            displayTime()
+            handler.postDelayed(runnable,10)
+
         }
+        handler.postDelayed(runnable,10)
+
+
     }
+}
+
+fun displayTime(){
+    var hours = time/360000
+
+
 }

bd4132dad75a2ce7d338b57bf12708c4b9c49800
BT_1N3_33 <sample@bths.edu>
2024-10-23 09:32:51 -0400

    Finished

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     private lateinit var timeDisplayer: TextView
@@ -16,35 +17,53 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pausebutton: ImageButton
     private lateinit var resetbutton: ImageButton
     var time = 0;
-
-
+    var clockRunning = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        timeDisplayer =
-
-
-
+        timeDisplayer = findViewById(R.id.showTime)
+        playbutton = findViewById(R.id.startbutton)
+        pausebutton = findViewById(R.id.pausebutton)
+        resetbutton = findViewById(R.id.resetbutton)
 
         val handler: Handler = Handler(Looper.getMainLooper())
         var runnable: Runnable = Runnable { }
         runnable = Runnable {
-            time++;
-            displayTime(time)
+
+            if (clockRunning){
+                time++;
+                displayTime()
+            }
             handler.postDelayed(runnable,10)
 
         }
         handler.postDelayed(runnable,10)
 
 
+        playbutton.setOnClickListener{
+            clockRunning = true;
+        }
+        pausebutton.setOnClickListener{
+            clockRunning = false;
+        }
+        resetbutton.setOnClickListener{
+            //clockRunning = false
+            time = 0;
+            displayTime()
+        }
     }
+
+    fun displayTime(){
+        var minutes = "0" + (time/6000).toString()
+
+        var seconds = if (((time/100)%60) > 10) ((time/100)%60).toString() else  "0" + ((time/100)%60).toString()
+
+        var deciseconds = if ((time % 100) > 10) (time % 100).toString() else  "0" + (time % 100).toString()
+        timeDisplayer.text = "$minutes : $seconds : $deciseconds"
+
+    }
+
+
 }
 
-fun displayTime(timeNow: Int){
-    var hours = (timeNow/360000).toInt()
-    var minutes = (timeNow - (timeNow % 6000))/6000
-    var deciseconds = timeNow % 100;
-
-
-}
